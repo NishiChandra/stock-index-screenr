c6fa02f9b853ea5f3a52915994104d9a7e3c07ae
Nishi Chandra <nishichandra97@gmail.com>
2025-05-05 22:56:38 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -2,14 +2,10 @@ import os
 import redis
 import logging
 
-DB_PATH = os.environ.get("DB_PATH", "index_data.duckdb")
-FMP_API_KEY = "tfE0OX2xq2vWRUd4Wr9Gc3IXpx5w9Vqi"  
-
-
 # Configuration
+DB_PATH = os.getenv("DB_PATH", "/app/app/data/index_data.duckdb")  # default fallback if not passed
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-DB_PATH = os.getenv("DB_PATH", "index_data.duckdb")
 
 # Logging
 logging.basicConfig(level=logging.INFO)
